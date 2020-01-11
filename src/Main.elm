fbadeab90d18f67618323b3b8af915640a34a047
Peter Damoc <pdamoc@gmail.com>
2020-01-11 10:03:26 +0200

    initial commit

---
+++ src/Main.elm
@@ -0,0 +1,9 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Ui exposing (..)
+
+
+main =
+    page [ text "Hello" ]
---
+++ src/Ui.elm
@@ -0,0 +1,12 @@
+module Ui exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+role =
+    attribute "role"
+
+
+page =
+    div [ role "page" ]
