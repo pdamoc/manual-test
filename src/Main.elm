8cbcfb71d23f130fbaab517d1b8d8ca69ad92861
Peter Damoc <pdamoc@gmail.com>
2020-01-11 14:47:09 +0200

    Added page 51, box 3

--- src/Main.elm
+++ src/Main.elm
@@ -7,70 +7,104 @@ import Ui exposing (..)
 
 
 page51Box1 =
-    stack
-        [ box
-            { color = "purple"
-            , title = "Explorare"
-            , content =
-                div []
-                    [ ol []
-                        [ li []
-                            [ span []
-                                [ text "Grupează ideile din rubrica "
-                                , em [] [ text "Explorare" ]
-                                , text " (pagina 49) într-un plan simplu, urmărind sugestiile din tabel"
-                                ]
-                            , div [ style "padding" "var(--s-1)" ]
-                                [ stackSmall
-                                    [ row
-                                        [ div [ class "col-3" ]
-                                            [ span [ class "box-color" ] [ text "Precizează cum începe acțiunea.", br [] [], text "(Cine? Ce face? Unde? Când?)" ]
-                                            ]
-                                        , div [ class "col-9 d-flex align-items-center" ] [ input [ type_ "text", value "Un domn vine în vizită la doamna Popescu cu ocazia onomasticii fiului ei." ] [] ]
+    box
+        { color = "purple"
+        , title = "Explorare"
+        , content =
+            div []
+                [ ol []
+                    [ li []
+                        [ span []
+                            [ text "Grupează ideile din rubrica "
+                            , em [] [ text "Explorare" ]
+                            , text " (pagina 49) într-un plan simplu, urmărind sugestiile din tabel"
+                            ]
+                        , div [ style "padding" "var(--s-1)" ]
+                            [ stackSmall
+                                [ row
+                                    [ div [ class "col-3" ]
+                                        [ span [ class "box-color" ] [ text "Precizează cum începe acțiunea.", br [] [], text "(Cine? Ce face? Unde? Când?)" ]
                                         ]
-                                    , row
-                                        [ div [ class "col-3" ]
-                                            [ div [ class "box-color" ]
-                                                [ span [] [ text "Prezintă ce se întâmplă:" ]
-                                                , ul []
-                                                    [ li [] [ text "cum se declanșează acțiunea;" ]
-                                                    , li [] [ text "cum se desfășoară acțiunea;" ]
-                                                    , li [] [ text "cum sunt depășite complicațiile" ]
-                                                    ]
+                                    , div [ class "col-9 d-flex align-items-center" ] [ input [ type_ "text", value "Un domn vine în vizită la doamna Popescu cu ocazia onomasticii fiului ei." ] [] ]
+                                    ]
+                                , row
+                                    [ div [ class "col-3" ]
+                                        [ div [ class "box-color" ]
+                                            [ span [] [ text "Prezintă ce se întâmplă:" ]
+                                            , ul []
+                                                [ li [] [ text "cum se declanșează acțiunea;" ]
+                                                , li [] [ text "cum se desfășoară acțiunea;" ]
+                                                , li [] [ text "cum sunt depășite complicațiile" ]
                                                 ]
                                             ]
-                                        , div [ class "col-9 d-flex align-items-center" ] [ textarea [] [] ]
                                         ]
-                                    , row
-                                        [ div [ class "col-3" ]
-                                            [ span [ class "box-color" ] [ text "Notează finalul întâmplării." ]
-                                            ]
-                                        , div [ class "col-9 d-flex align-items-center" ] [ input [ type_ "text" ] [] ]
+                                    , div [ class "col-9 d-flex align-items-center" ] [ textarea [] [] ]
+                                    ]
+                                , row
+                                    [ div [ class "col-3" ]
+                                        [ span [ class "box-color" ] [ text "Notează finalul întâmplării." ]
                                         ]
+                                    , div [ class "col-9 d-flex align-items-center" ] [ input [ type_ "text" ] [] ]
                                     ]
                                 ]
                             ]
-                        , li []
-                            [ text "Formulează o idee care să completeze, prin detalii, prima idee principală de la exercițiul anterior." ]
+                        ]
+                    , li []
+                        [ text "Formulează o idee care să completeze, prin detalii, prima idee principală de la exercițiul anterior." ]
+                    ]
+                ]
+        }
+
+
+page51Box2 =
+    box
+        { color = "blue"
+        , title = "Aplicații"
+        , content =
+            div []
+                [ ol []
+                    [ li []
+                        [ span []
+                            [ text "Alcătuiește planul dezvoltat de idei, adăugând detalii pentru fiecare idee principală din planul simplu."
+                            ]
+                        , div [ style "padding" "var(--s-1)", style "padding-left" "var(--s0)" ]
+                            []
+                        ]
+                    , li []
+                        [ span [] [ text "Evaluează planul dezvoltat al colegului/colegei de bancă, pe baza următoarelor criterii:" ]
+                        , div [ style "padding" "var(--s-1)" ]
+                            [ table [ attribute "data-color" "blue" ]
+                                [ tr [] [ th [] [ text "Criterii" ], th [] [ text "Da" ], th [] [ text "Nu" ] ]
+                                , tr [] [ td [] [ text "A fost dezvoltată fiecare idee principală, adăugându-se detalii relevante." ], td [] [], td [] [] ]
+                                , tr [] [ td [] [ text "A fost respectată ordinea în care apar în text ideile principale și secundare." ], td [] [], td [] [] ]
+                                , tr [] [ td [] [ text "Exprimarea este clară și corectă, fără greșeli gramaticale, de ortografie sau de punctuație." ], td [] [], td [] [] ]
+                                ]
+                            ]
                         ]
                     ]
-            }
-        , box
-            { color = "orange"
-            , title = "Repere"
-            , content =
-                div []
-                    [ p [] [ span [ class "box-color" ] [ text "Ideile secundare" ], text " prezintă amănunte legate de ideea principală, completând-o." ]
-                    , p [] [ span [ class "box-color" ] [ text "Planul dezvoltat de idei" ], text " cuprinde ideile principale și ideile secundare corespunzătoare acestora, în ordinea în care apar în text." ]
-                    ]
-            }
-        , box
-            { color = "green"
-            , title = "Repere"
-            , content =
-                p [] [ text " Lucrați în grupe de câte 4 – 5 elevi. Schițați o bandă desenată în care să surprindeți cele mai importante secvențe ale schiței ", em [] [ text "Vizită" ], text "...." ]
-            }
-        ]
+                ]
+        }
+
+
+page51Box3 =
+    box
+        { color = "orange"
+        , title = "Repere"
+        , content =
+            div []
+                [ p [] [ span [ class "box-color" ] [ text "Ideile secundare" ], text " prezintă amănunte legate de ideea principală, completând-o." ]
+                , p [] [ span [ class "box-color" ] [ text "Planul dezvoltat de idei" ], text " cuprinde ideile principale și ideile secundare corespunzătoare acestora, în ordinea în care apar în text." ]
+                ]
+        }
+
+
+page51Box4 =
+    box
+        { color = "green"
+        , title = "Repere"
+        , content =
+            p [] [ text " Lucrați în grupe de câte 4 – 5 elevi. Schițați o bandă desenată în care să surprindeți cele mai importante secvențe ale schiței ", em [] [ text "Vizită" ], text "...." ]
+        }
 
 
 page51 =
@@ -78,7 +112,12 @@ page51 =
         content =
             div []
                 [ h2 [ style "color" "var(--color-orange)" ] [ strongText "Lecția 3.", text " Planul dezvoltat de idei" ]
-                , page51Box1
+                , stackSmall
+                    [ page51Box1
+                    , page51Box2
+                    , page51Box3
+                    , page51Box4
+                    ]
                 ]
     in
     page
